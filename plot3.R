d7e98e5a3238e2ed469ad6d794c66669bbf55e51
rbhowmick <rajb007@me.com>
2017-01-02 04:15:52 -0500

    Second Commit

--- plot3.R
+++ plot3.R
@@ -5,9 +5,10 @@ pow_cons_subset <- subset(pow_cons, datetime > strptime("2007-01-31 23:59:00 EST
 pow_cons_subset$Sub_metering_1 <- as.numeric(as.character(pow_cons_subset$Sub_metering_1))
 pow_cons_subset$Sub_metering_2 <- as.numeric(as.character(pow_cons_subset$Sub_metering_2))
 pow_cons_subset$Sub_metering_3 <- as.numeric(as.character(pow_cons_subset$Sub_metering_3))
+
+png(file="plot3.png", width=480, height=480)
 with(pow_cons_subset,plot(datetime,Sub_metering_1,type="l",ylab="Energy sub metering"))
 with(pow_cons_subset,lines(datetime,Sub_metering_2,col="red"))
 with(pow_cons_subset,lines(datetime,Sub_metering_3,col="blue"))
 legend("topright", lty=c(1,1), col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(png, file = "plot3.png")
 dev.off()
